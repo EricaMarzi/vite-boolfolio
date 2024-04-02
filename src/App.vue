19b9cff53bd3032b60e0f2e7681dcbec6c4678cd
EricaMarzi <erica.marzi.1993@gmail.com>
2024-04-02 16:15:29 +0200

    img size + color title

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script>
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+import PostsList from './components/posts/PostsList.vue';
 const endpoint = 'http://localhost:8000/api/posts/'
 
 export default {
   name: 'Portfolio',
   data: () => ({ posts: [] }),
-  components: { AppHeader },
+  components: { AppHeader, PostsList },
   methods: {
     fetchPosts() {
       axios.get(endpoint).then(res => {
@@ -24,30 +25,8 @@ export default {
 
   <AppHeader />
 
-
-  <!--MAIN-->
   <main class="container">
-    <h1 class="my-5">I nostri post</h1>
-    <div class="row">
-      <div v-if="posts.length">
-        <div class="card mb-4 " v-for="post in posts" :key="post.id">
-          <h5 class="card-header">{{ post.title }}</h5>
-          <div class="card-body clearfix">
-            <img v-if="post.image" :src="post.image" :alt="post.title" class="img-fluid float-start me-2">
-            <p class="card-text">{{ post.content.slice(0, 400) }}</p>
-          </div>
-          <div class="card-footer d-flex justify-content-between">
-            <!--da formattare quando metti i componenti-->
-            <div> Pubblicato il {{ post.created_at }}</div>
-
-            <!-- <div>{{ post.category.label }}</div> -->
-
-            <a href="#" class="btn btn-primary">Continua a leggere</a>
-          </div>
-        </div>
-      </div>
-      <h2 v-else>Non ci sono post</h2>
-    </div>
+    <PostsList :posts="posts" />
   </main>
 
 
---
+++ src/components/posts/PostsList.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    name: 'PostsList',
+    props: { posts: Array }
+}
+</script>
+
+<template>
+    <h1 class="my-5 title">I nostri post</h1>
+    <div class="row">
+        <div v-if="posts.length">
+            <div class="card mb-4 " v-for="post in posts" :key="post.id">
+                <h5 class="card-header">{{ post.title }}</h5>
+                <div class="card-body clearfix">
+                    <img v-if="post.image" :src="post.image" :alt="post.title" class="img-fluid float-start me-2">
+                    <p class="card-text">{{ post.content.slice(0, 800) }}...</p>
+                </div>
+                <div class="card-footer d-flex justify-content-between">
+
+                    <!--da formattare quando metti i componenti-->
+                    <div> Pubblicato il {{ post.created_at }}</div>
+
+                    <!-- <div>{{ post.category.label }}</div> -->
+
+                    <a href="#" class="btn btn-primary">Continua a leggere</a>
+                </div>
+            </div>
+        </div>
+        <h2 v-else>Non ci sono post</h2>
+    </div>
+</template>
+
+<style lang='scss' scoped>
+.title {
+    color: #00b6b3;
+}
+
+img {
+    width: 200px;
+}
+</style>
