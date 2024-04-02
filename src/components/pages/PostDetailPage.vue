4556439447486a7fbddc3b8300158af16298ec75
EricaMarzi <erica.marzi.1993@gmail.com>
2024-04-02 19:53:43 +0200

    card detail

--- src/components/pages/PostDetailPage.vue
+++ src/components/pages/PostDetailPage.vue
@@ -1,11 +1,34 @@
 <script>
+import axios from 'axios';
+import AppLoader from '../AppLoader.vue';
+import PostCard from '../posts/PostCard.vue';
+const endpoint = 'http://localhost:8000/api/posts/'
 export default {
-    name: 'PostDetailPage'
+    name: 'PostDetailPage',
+    components: { PostCard, AppLoader },
+    data: () => ({
+        post: null,
+        isLoading: false
+    }),
+    methods: {
+        getPost() {
+            this.isLoading = true;
+            axios.get(endpoint + this.$route.params.id)
+                .then(res => { this.post = res.data; })
+                .catch(err => { console.error(err.message) })
+                .then(() => { this.isLoading = false })
+        }
+    },
+    created() {
+        this.getPost();
+    }
 }
 </script>
 
 <template>
     <h2 class="mt-5">Dettaglio post</h2>
+    <AppLoader v-if="isLoading && !post" />
+    <PostCard v-if="!isLoading && post" :post="post" />
 </template>
 
 <style lang='scss' scoped>
