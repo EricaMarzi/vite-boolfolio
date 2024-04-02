0da66e0b208a541b8fb83acb8480b69bc81c1a10
EricaMarzi <erica.marzi.1993@gmail.com>
2024-04-02 16:49:04 +0200

    PostCard component add

---
+++ src/components/posts/PostCard.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    name: 'PostCard',
+    props: { post: Object }
+}
+</script>
+
+<template>
+    <div class="card mb-4">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h5>{{ post.title }}</h5>
+            <div class="badge rounded-pill text-bg-success">
+                {{ post.category.label }}</div>
+        </div>
+
+        <div class="card-body clearfix">
+            <img v-if="post.image" :src="post.image" :alt="post.title" class="img-fluid float-start me-2">
+            <p class="card-text">{{ post.content.slice(0, 800) }}...</p>
+        </div>
+
+
+        <div class="card-footer d-flex justify-content-between">
+
+            <div class="d-flex gap-3">
+                <!--Tag non ancora presenti nella chiamata api-->
+                <!-- <div>
+                    <div class="badge rounded-pill text-bg-success">
+                        {{ post.category.tag }}</div>
+                    </div> -->
+                <!--da formattare quando metti i componenti-->
+                <div> Pubblicato il {{ post.created_at }}</div>
+            </div>
+
+            <a href="#" class="btn btn-bg-custom">Continua a leggere</a>
+        </div>
+    </div>
+</template>
+
+<style lang='scss' scoped>
+img {
+    width: 200px;
+}
+
+.btn-bg-custom {
+    background-color: #00b6b3;
+    color: white;
+    font-weight: 600;
+}
+</style>
--- src/components/posts/PostsList.vue
+++ src/components/posts/PostsList.vue
@@ -1,6 +1,8 @@
 <script>
+import PostCard from './PostCard.vue';
 export default {
     name: 'PostsList',
+    components: { PostCard },
     props: { posts: Array }
 }
 </script>
@@ -10,35 +12,7 @@ export default {
     <h1 class="my-5 title">I nostri post</h1>
     <div class="row">
         <div v-if="posts.length">
-            <div class="card mb-4 " v-for="post in   posts  " :key="post.id">
-
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <h5>{{ post.title }}</h5>
-                    <div class="badge rounded-pill text-bg-success">
-                        {{ post.category.label }}</div>
-                </div>
-
-                <div class="card-body clearfix">
-                    <img v-if="post.image" :src="post.image" :alt="post.title" class="img-fluid float-start me-2">
-                    <p class="card-text">{{ post.content.slice(0, 800) }}...</p>
-                </div>
-
-
-                <div class="card-footer d-flex justify-content-between">
-
-                    <div class="d-flex gap-3">
-                        <!--Tag non ancora presenti nella chiamata api-->
-                        <!-- <div>
-                            <div class="badge rounded-pill text-bg-success">
-                                {{ post.category.tag }}</div>
-                            </div> -->
-                        <!--da formattare quando metti i componenti-->
-                        <div> Pubblicato il {{ post.created_at }}</div>
-                    </div>
-
-                    <a href="#" class="btn btn-bg-custom">Continua a leggere</a>
-                </div>
-            </div>
+            <PostCard v-for="post in   posts  " :key="post.id" :post="post" />
         </div>
         <h2 v-else>Non ci sono post pubblicati</h2>
     </div>
@@ -47,15 +21,5 @@ export default {
 <style lang='scss' scoped>
 .title {
     color: #00b6b3;
-}
-
-img {
-    width: 200px;
-}
-
-.btn-bg-custom {
-    background-color: #00b6b3;
-    color: white;
-    font-weight: 600;
 }
 </style>
