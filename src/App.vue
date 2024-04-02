c79b392eec2968feb08fb11ba199ffc67f1bee0e
EricaMarzi <erica.marzi.1993@gmail.com>
2024-04-02 19:23:04 +0200

    import cancel

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import HomePage from './components/pages/HomePage.vue';
 const endpoint = 'http://localhost:8000/api/posts/'
 
 export default {
   name: 'Portfolio',
-  components: { AppHeader, HomePage },
+  components: { AppHeader },
 }
 </script>
 
 <template>
 
   <AppHeader />
-  <!-- <HomePage /> -->
   <main class="container">
     <RouterView />
   </main>
