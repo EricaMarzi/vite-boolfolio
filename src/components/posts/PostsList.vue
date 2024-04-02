8abefe33776a14a0e9a67ac041fd596b7451025b
EricaMarzi <erica.marzi.1993@gmail.com>
2024-04-02 16:42:20 +0200

    Component post list

--- src/components/posts/PostsList.vue
+++ src/components/posts/PostsList.vue
@@ -6,27 +6,41 @@ export default {
 </script>
 
 <template>
+
     <h1 class="my-5 title">I nostri post</h1>
     <div class="row">
         <div v-if="posts.length">
-            <div class="card mb-4 " v-for="post in posts" :key="post.id">
-                <h5 class="card-header">{{ post.title }}</h5>
+            <div class="card mb-4 " v-for="post in   posts  " :key="post.id">
+
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <h5>{{ post.title }}</h5>
+                    <div class="badge rounded-pill text-bg-success">
+                        {{ post.category.label }}</div>
+                </div>
+
                 <div class="card-body clearfix">
                     <img v-if="post.image" :src="post.image" :alt="post.title" class="img-fluid float-start me-2">
                     <p class="card-text">{{ post.content.slice(0, 800) }}...</p>
                 </div>
+
+
                 <div class="card-footer d-flex justify-content-between">
 
-                    <!--da formattare quando metti i componenti-->
-                    <div> Pubblicato il {{ post.created_at }}</div>
+                    <div class="d-flex gap-3">
+                        <!--Tag non ancora presenti nella chiamata api-->
+                        <!-- <div>
+                            <div class="badge rounded-pill text-bg-success">
+                                {{ post.category.tag }}</div>
+                            </div> -->
+                        <!--da formattare quando metti i componenti-->
+                        <div> Pubblicato il {{ post.created_at }}</div>
+                    </div>
 
-                    <!-- <div>{{ post.category.label }}</div> -->
-
-                    <a href="#" class="btn btn-primary">Continua a leggere</a>
+                    <a href="#" class="btn btn-bg-custom">Continua a leggere</a>
                 </div>
             </div>
         </div>
-        <h2 v-else>Non ci sono post</h2>
+        <h2 v-else>Non ci sono post pubblicati</h2>
     </div>
 </template>
 
@@ -37,5 +51,11 @@ export default {
 
 img {
     width: 200px;
+}
+
+.btn-bg-custom {
+    background-color: #00b6b3;
+    color: white;
+    font-weight: 600;
 }
 </style>
